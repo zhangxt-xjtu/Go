510a0ef8ca70ad80b72713f2bb9b0610a8d5df35
zhangxt-xjtu <zhangxinteng619@gmail.com>
2024-04-02 15:42:47 +0800

    Your branch is up to date with 'origin/main'. 	modified:   Learning/structs/shape.go 	modified:   Learning/structs/shape_test.go

--- Learning/structs/shape_test.go
+++ Learning/structs/shape_test.go
@@ -14,22 +14,24 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf(("got %.2f want %.2f"), got, want)
-		}
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf(("%#v got %.2f want %.2f"), tt.shape, got, tt.want)
+			}
+		})
+	}
 
 }
