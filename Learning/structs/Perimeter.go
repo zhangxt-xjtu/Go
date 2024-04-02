c7e3a2e96a4d88207731195d01796cbfac6ed780
zhangxt-xjtu <zhangxinteng619@gmail.com>
2024-04-02 15:21:00 +0800

    deleted:    Learning/structs/Perimeter.go 	deleted:    Learning/structs/Perimeter_test.go 	new file:   Learning/structs/shape.go 	new file:   Learning/structs/shape_test.go

--- Learning/structs/Perimeter.go
+++
@@ -1,9 +0,0 @@
-package structs
-
-func Perimeter(width, height float64) float64 {
-	return 2 * (height + width)
-}
-
-func Area(width, height float64) float64 {
-	return width * height
-}
---
+++ Learning/structs/shape.go
@@ -0,0 +1,28 @@
+package structs
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
