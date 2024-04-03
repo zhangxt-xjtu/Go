dd9aa10ad48fcfe5b6050995772901d0948e4cb3
zhangxt-xjtu <zhangxinteng619@gmail.com>
2024-04-03 14:42:45 +0800

    modified:   Learning/pointers/wallet.go 	modified:   Learning/pointers/wallet_test.go

--- Learning/pointers/wallet.go
+++ Learning/pointers/wallet.go
@@ -1,13 +1,29 @@
 package pointers
 
-type Wallet struct {
-	balance int
+import "fmt"
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(money int) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(money Bitcoin) {
 	w.balance += money
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(money Bitcoin) {
+	w.balance -= money
 }
--- Learning/pointers/wallet_test.go
+++ Learning/pointers/wallet_test.go
@@ -6,14 +6,22 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
-
-	wallet.Deposit(10)
-
-	got := wallet.Balance()
-	want := 10
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+	CheckBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		CheckBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		CheckBalance(t, wallet, Bitcoin(10))
+	})
+
 }
