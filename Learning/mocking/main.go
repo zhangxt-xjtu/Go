5a4878df1a435886c5b01ec74d4560e661bca8ae
zhangxt-xjtu <zhangxinteng619@gmail.com>
2024-04-09 15:11:24 +0800

    modified:   Learning/mocking/countdown_test.go 	modified:   Learning/mocking/main.go

--- Learning/mocking/main.go
+++ Learning/mocking/main.go
@@ -3,8 +3,44 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
+	"time"
 )
 
-func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second}
+	Countdown(os.Stdout, sleeper)
 }
