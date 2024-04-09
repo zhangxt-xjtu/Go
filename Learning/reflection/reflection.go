920f9fd637a35dddb75a2cb6254877a061b2c507
zhangxt-xjtu <zhangxinteng619@gmail.com>
2024-04-09 19:23:09 +0800

    new file:   Learning/reflection/reflection.go 	new file:   Learning/reflection/reflection_test.go

---
+++ Learning/reflection/reflection.go
@@ -0,0 +1,17 @@
+package reflect
+
+import (
+	"reflect"
+)
+
+func walk(x interface{}, fn func(string)) {
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+	}
+
+}
