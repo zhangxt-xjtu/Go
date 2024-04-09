d67ac39c41dfffb09f71058c990baa0dc4fb63fb
zhangxt-xjtu <zhangxinteng619@gmail.com>
2024-04-09 14:17:35 +0800

    modified:   Learning/di/di.go 	modified:   Learning/di/di_test.go

--- Learning/di/di.go
+++ Learning/di/di.go
@@ -1,10 +1,23 @@
-package di
+package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"net/http"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	err := http.ListenAndServe(":8000", http.HandlerFunc(MyGreeterHandler))
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
